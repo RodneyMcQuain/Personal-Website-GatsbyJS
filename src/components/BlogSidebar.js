0b47d87bce3951411e40ff0df2a88ad598c9c37c
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-13 21:29:22 -0400

    Add sidebar to a blog post which contains other posts to go to

---
+++ src/components/BlogSidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const BlogSidebar = ({posts}) => {
+    return (
+        <ul className="blog-sidebar -curved-border padding-container">
+            <li><Link to={'blog-posts'}>
+                <span>All Posts</span>
+            </Link></li>
+            {posts.map(post => {
+                    const { node } = post;
+                    const { title, path } = node.frontmatter;
+
+                    return (
+                        <>
+                            <hr />
+                            <li><Link to={path}>
+                                <span>{title}</span>
+                            </Link></li>
+                        </>
+                    );
+                })
+            }
+        </ul>
+    );
+};
+
+export default BlogSidebar;
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -3,35 +3,51 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import FullDate from '../components/FullDate';
 import InViewAnimation from '../components/InViewAnimation';
+import BlogSidebar from '../components/BlogSidebar';
 
 export default function Template({ data }) {
-    const post = data.markdownRemark;
+    const post = data.thisPost;
 
     return (
         <Layout>
             <InViewAnimation>
                 <div className="container">
-                    <FullDate style="left-align blog-post-date-spacing" date={post.frontmatter.date} />
-                    <div className="page-header">
-                        <h1 className="left-align blog-title">{post.frontmatter.title}</h1>
+                    <div className="col-md-9">
+                        <FullDate style="left-align blog-post-date-spacing" date={post.frontmatter.date} />
+                        <div className="page-header">
+                            <h1 className="left-align blog-title">{post.frontmatter.title}</h1>
+                        </div>
+                        <div className="markdown-body" dangerouslySetInnerHTML={{ __html: post.html }} />
                     </div>
-                    <div className="markdown-body" dangerouslySetInnerHTML={{ __html: post.html }} />
+                    <div className="hidden-sm col-md-3">
+                        <BlogSidebar posts={data.recentPosts.edges} />
+                    </div>
                 </div>
             </InViewAnimation>
-    </Layout>
+        </Layout>
   );
 }
 
 export const pageQuery = graphql`
-  query BlogPostByPath($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      frontmatter {
-        path
-        title
-        author
-        date
-      }
+    query BlogPostByPath($path: String!) {
+        thisPost: markdownRemark(frontmatter: { path: { eq: $path } }) {
+            html
+            frontmatter {
+                path
+                title
+                date
+            }
+        }
+        recentPosts: allMarkdownRemark(limit: 5) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        path
+                        title
+                    }
+                }
+            }
+        }
     }
-  }
 `
