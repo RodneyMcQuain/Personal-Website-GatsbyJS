7e88d5c616b5b69c5672a46c0cd797a61c5231fe
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-15 20:18:57 -0400

    add preloader before page loads

---
+++ src/components/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => (
+    <div id="preloader-overlay">
+        <div id="preloader-spinner">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+);
+
+export default Preloader;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import Preloader from './Preloader';
 import '../styles/stylesheet.scss';
 
-const Layout = ({children}) => (
-    <>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-    </>
-);
+const Layout = ({children}) => {
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        setIsLoading(false);
+    }, []);
+
+    return isLoading 
+        ? <Preloader />
+        : (
+            <>
+                <Navbar />
+                <main>{children}</main>
+                <Footer />
+            </>
+        );
+};
 
 export default Layout;
