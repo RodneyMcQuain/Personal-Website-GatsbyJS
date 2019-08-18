3312dde0e3ac2e16b6efc925b3b84a62a52df8ef
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-18 15:38:43 -0400

    add scrolling to correct section on mount by hash

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from './Banner';
 import AboutMe from './AboutMe';
 import Education from '../components/Education/Education';
@@ -6,15 +6,28 @@ import Projects from '../components/Projects/Projects';
 import Skills from '../components/Skills/Skills';
 import Contact from './Contact/Contact';
 
-const Portfolio = () => (
-    <>
-        <Banner />
-        <AboutMe />
-        <Education />
-        <Projects />
-        <Skills />
-        <Contact />
-    </>
-);
+const Portfolio = () => {
+    useEffect(scrollToCurrentHash, []);
+
+    return (
+        <>
+            <Banner />
+            <AboutMe />
+            <Education />
+            <Projects />
+            <Skills />
+            <Contact />
+        </>
+    );
+};
+
+const scrollToCurrentHash = () => {
+    const hash = window.location.hash;
+    if (hash) {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        element.scrollIntoView();
+    }
+};
 
 export default Portfolio;
