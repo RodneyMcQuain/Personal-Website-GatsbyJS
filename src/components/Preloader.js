c92706b8a10b742137a9d4f7690edfb372e05eb4
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-18 15:57:55 -0400

    update sr-only to use className instead of class

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Preloader = () => (
     <div id="preloader-overlay">
         <div id="preloader-spinner">
-            <span class="sr-only">Loading...</span>
+            <span className="sr-only">Loading...</span>
         </div>
     </div>
 );
