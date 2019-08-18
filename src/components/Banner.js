486a2fe726f68db66a1b2f3a0e7f262acc716d43
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-18 16:52:46 -0400

    update fade elements to use className instead of class

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,12 +20,12 @@ const Banner = () => {
       id="banner-img"
     >
       <div className="col-xl-12 col-md-12 col-sm-12 col-xs-12" id="banner-container">
-        <h1 id="banner-header" class={fadeHeader}>Rodney McQuain</h1>
-        <p id="banner-text" class={fadeTextAndButton}>
+        <h1 id="banner-header" className={fadeHeader}>Rodney McQuain</h1>
+        <p id="banner-text" className={fadeTextAndButton}>
           Hello, I am a computer science major looking for an opportunity
           to test my skills and grow as a developer.
         </p>
-        <div id="banner-btn-container" class={fadeTextAndButton}>
+        <div id="banner-btn-container" className={fadeTextAndButton}>
           <a id="banner-btn" className="-curved-border" href="#about-me">
             Learn About Me
           </a>
