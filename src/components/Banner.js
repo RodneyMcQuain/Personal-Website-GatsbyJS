40f2735c70a782c1425e675c09af5afc7e3466c7
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-19 21:04:59 -0400

    clean: clean up timeouts on unmount

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,38 +1,46 @@
 import React, { useEffect, useState } from 'react';
 
-const backgroundImage = `linear-gradient(
-  rgba(0, 0, 0, 0.5),
-  rgba(0, 0, 0, 0.5)
-  ), url(code.jpg)`;
+const backgroundImage = `
+    linear-gradient(
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+    ), 
+    url(code.jpg)
+`;
 
 const Banner = () => {
-  const [fadeHeader, setFadeHeader] = useState("banner-hide");
-  const [fadeTextAndButton, setFadeTextAndButton] = useState("banner-hide");
+    const [fadeHeader, setFadeHeader] = useState("banner-hide");
+    const [fadeTextAndButton, setFadeTextAndButton] = useState("banner-hide");
 
-  useEffect(() => {
-    setTimeout(() => setFadeHeader("banner-appear"), 250);
-    setTimeout(() => setFadeTextAndButton("banner-appear"), 1500);
-  }, []);
+    useEffect(() => {
+        const fadeHeaderTimer = setTimeout(() => setFadeHeader("banner-appear"), 250);
+        const fadeTextAndButtonTimer = setTimeout(() => setFadeTextAndButton("banner-appear"), 1500);
+  
+        return () => {
+            clearTimeout(fadeHeaderTimer);
+            clearTimeout(fadeTextAndButtonTimer);        
+        }
+    }, []);
 
-  return (
-    <header 
-      style={{ backgroundImage: backgroundImage }} 
-      id="banner-img"
-    >
-      <div className="col-xl-12 col-md-12 col-sm-12 col-xs-12" id="banner-container">
-        <h1 id="banner-header" className={fadeHeader}>Rodney McQuain</h1>
-        <p id="banner-text" className={fadeTextAndButton}>
-          Hello, I am a computer science major looking for an opportunity
-          to test my skills and grow as a developer.
-        </p>
-        <div id="banner-btn-container" className={fadeTextAndButton}>
-          <a id="banner-btn" className="-curved-border" href="#about-me">
-            Learn About Me
-          </a>
-        </div>
-      </div>
-    </header>
-  )
+    return (
+        <header 
+        style={{ backgroundImage: backgroundImage }} 
+        id="banner-img"
+        >
+            <div className="col-xl-12 col-md-12 col-sm-12 col-xs-12" id="banner-container">
+                <h1 id="banner-header" className={fadeHeader}>Rodney McQuain</h1>
+                <p id="banner-text" className={fadeTextAndButton}>
+                    Hello, I am a computer science major looking for an opportunity
+                    to test my skills and grow as a developer.
+                </p>
+                <div id="banner-btn-container" className={fadeTextAndButton}>
+                    <a id="banner-btn" className="-curved-border" href="#about-me">
+                        Learn About Me
+                    </a>
+                </div>
+            </div>
+        </header>
+    )
 };
 
 export default Banner;
