a9eed102c68c66f14cdf45a40b6340b25dd8b88e
Rodney McQuain <rodneymcquain95@gmail.com>
2019-08-31 14:46:51 -0400

    feat: add basic open graph attributes for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,46 @@
 module.exports = {
-  siteMetadata: {
-    title: `Portfolio`,
-    description: ``,
-    author: `Rodney McQuain`,
-  },
-  plugins: [
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-      },
+    siteMetadata: {
+        title: `Rodney McQuain`,
+        description: `Yet another computer science student, come learn about me.`,
+        author: `Rodney McQuain`,
+        url: `https://rodney-mcquain.netlify.com`,
+        image: `/code.jpg`,
     },
-    `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/data`,
-        name: `data`
-      }
-    },
-    `gatsby-plugin-catch-links`,
-    { 
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: `pages`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-prismjs`,
-        ]
-      }
-    }
-  ],
+    plugins: [
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-remark`,
+        {
+        resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        `gatsby-transformer-json`,
+        {
+        resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/data`,
+                name: `data`
+            },
+        },
+        `gatsby-plugin-catch-links`,
+        { 
+        resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: `pages`,
+            },
+        },
+        {
+        resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                `gatsby-remark-prismjs`,
+                ]
+            },
+        },
+    ],
 }
