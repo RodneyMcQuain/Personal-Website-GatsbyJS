ef45b1db2eff2fcf09ae7bdff24276eadebba99d
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-25 02:35:30 -0500

    feat: make the external links in the footer only icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Footer = () => (
     <footer className="dark-section">
-        <div className="row">
-            <div className="col-sm-6">
-                <p>© {new Date().getFullYear()} Copyright: Rodney McQuain</p>
-            </div>
+        <div className="container">
+            <p className="footer-copyright">© {new Date().getFullYear()} Copyright: Rodney McQuain</p>
 
-            <div className="col-sm-6">
-                <a className="btn" href="https://github.com/RodneyMcQuain/" aria-label="Go to my GitHub">
-                    <span className="fa fa-github"><span> My GitHub</span></span>
+            <div>
+                <a className="btn footer-icon" href="https://github.com/RodneyMcQuain/" aria-label="Go to my GitHub" title="My GitHub">
+                    <span className="fa fa-github"></span>
                 </a>
-                <a className="btn" href="https://www.linkedin.com/in/rodneymcquain/" aria-label="Go to my LinkedIn">
-                    <span className="fa fa-linkedin"><span> My LinkedIn</span></span>
+                <a className="btn footer-icon" href="https://www.linkedin.com/in/rodneymcquain/" aria-label="Go to my LinkedIn" title="My LinkedIn">
+                    <span className="fa fa-linkedin"></span>
                 </a>
             </div>
         </div>
