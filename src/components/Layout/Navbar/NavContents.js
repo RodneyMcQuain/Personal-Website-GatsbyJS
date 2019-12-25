677ff8e932b86b9e4017dcbf6a1e6e7ecc1401a3
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-25 18:47:46 -0500

    feat: add icons to all navbar items

--- src/components/Layout/Navbar/NavContents.js
+++ src/components/Layout/Navbar/NavContents.js
@@ -12,10 +12,10 @@ const NavContents = ({isOpen}) => (
         currentClassName={ACTIVE_CLASS_NAME}
     >
         <NavBrand />
-        <NavItem text="About Me" url="#about-me" />
-        <NavItem text="Education" url="#education" />
-        <NavItem text="Projects" url="#projects" />
-        <NavItem text="Skills" url="#skills" />
+        <NavItem text="About Me" url="#about-me" icon="fa fa-user" />
+        <NavItem text="Education" url="#education" icon="fa fa-graduation-cap" />
+        <NavItem text="Projects" url="#projects" icon="fa fa-briefcase" />
+        <NavItem text="Skills" url="#skills" icon="fa fa-list-alt" />
         <NavItem text="Blog" url="/blog-posts" icon="fa fa-pencil" isSeparatePage={true} />
         <NavItem text="Resume" url="/resume" icon="fa fa-file-pdf-o" isSeparatePage={true} />
         <NavItem text="Contact" url="/contact" icon="fa fa-envelope" isSeparatePage={true} />
