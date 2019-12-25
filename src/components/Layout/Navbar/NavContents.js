1f9b28c4f6620b2641bd3a0d85249f77ee3abe5c
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-25 18:23:43 -0500

    feat: re-arrange navbar item ordering

--- src/components/Layout/Navbar/NavContents.js
+++ src/components/Layout/Navbar/NavContents.js
@@ -12,13 +12,13 @@ const NavContents = ({isOpen}) => (
         currentClassName={ACTIVE_CLASS_NAME}
     >
         <NavBrand />
-        <NavItem text="Blog" url="/blog-posts" icon="fa fa-pencil" isSeparatePage={true} />
         <NavItem text="About Me" url="#about-me" />
         <NavItem text="Education" url="#education" />
         <NavItem text="Projects" url="#projects" />
         <NavItem text="Skills" url="#skills" />
-        <NavItem text="Contact" url="/contact" icon="fa fa-envelope" isSeparatePage={true} />
+        <NavItem text="Blog" url="/blog-posts" icon="fa fa-pencil" isSeparatePage={true} />
         <NavItem text="Resume" url="/resume" icon="fa fa-file-pdf-o" isSeparatePage={true} />
+        <NavItem text="Contact" url="/contact" icon="fa fa-envelope" isSeparatePage={true} />
     </Scrollspy>
 );
 
