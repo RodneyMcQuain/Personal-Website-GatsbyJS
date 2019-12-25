104928de5a700446a14f2d5b3445963d8e0f401c
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-25 18:53:22 -0500

    fix: scroll to header based on hash on initial load

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,14 +5,27 @@ import Education from './Education/Education';
 import Projects from './Projects/Projects';
 import Skills from './Skills/Skills';
 
-const Portfolio = () => (
-    <>
-        <Banner />
-        <AboutMe />
-        <Education />
-        <Projects />
-        <Skills />
-    </>
-);
+const Portfolio = () => {
+    useEffect(scrollToCurrentHash, []);
+
+    return (
+        <>
+            <Banner />
+            <AboutMe />
+            <Education />
+            <Projects />
+            <Skills />
+        </>
+    );
+};
+
+const scrollToCurrentHash = () => {
+    const hash = window.location.hash;
+    if (hash) {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        element.scrollIntoView();
+    }
+};
 
 export default Portfolio;
