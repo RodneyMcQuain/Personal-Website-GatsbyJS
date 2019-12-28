383d3549b9c5dde2fa27da23987f8ce1e2a97ef2
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-28 09:32:38 -0500

    clean: update deprecated boundActionCreators

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
 const path = require('path')
 
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators
+exports.createPages = ({ action, graphql }) => {
+    const { createPage } = action
 
-  const postTemplate = path.resolve('src/templates/BlogPost.js')
+    const postTemplate = path.resolve('src/templates/BlogPost.js')
 
-  return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            id
-            frontmatter {
-              path
-              title
-              date
-              author
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        html
+                        id
+                        frontmatter {
+                            path
+                            title
+                            date
+                            author
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  `).then(res => {
-    if (res.errors)
-      return Promise.reject(res.errors)
+    `).then(res => {
+        if (res.errors)
+            return Promise.reject(res.errors);
 
-    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: postTemplate,
-      })
-    })
-  })
+        res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.frontmatter.path,
+                component: postTemplate,
+            });
+        });
+    });
 }
