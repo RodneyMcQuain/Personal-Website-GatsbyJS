8721ff8690ff0b15b518b02f4b2a95c8903d0a1e
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-28 16:19:17 -0500

    fix: make blog menu button not cover footer buttons

--- src/components/Blog/ResponsiveBlogMenu.js
+++ src/components/Blog/ResponsiveBlogMenu.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BlogMenu from './BlogMenu';
 import NavButton from '../NavButton';
 import CloseOnOutsideClick from '../CloseOnOutsideClick';
+import { MAX_FOOTER_HEIGHT } from '../../styles/helpers/_variables.scss';
+import { useAddCssClass } from '../../services/useAddCssClass';
+import { useFunctionThrottle } from '../../services/useFunctionThrottle';
+import { getViewportSize } from '../../services/getViewportSize';
 
 const ResponsiveBlogMenu = ({posts, currentPostName, tableOfContents}) => {
     const [isOpen, setIsOpen] = useState(false);
+    const mightBeFooterInView = useOffsetByFooter();
 
     return (
         <CloseOnOutsideClick setIsOpen={setIsOpen}> 
@@ -12,12 +17,30 @@ const ResponsiveBlogMenu = ({posts, currentPostName, tableOfContents}) => {
             <NavButton
                 isOpen={isOpen}
                 setIsOpen={setIsOpen}
-                extraClasses="btn blog-menu-btn hidden-md hidden-lg"
+                extraClasses={`btn blog-menu-btn hidden-md hidden-lg ${mightBeFooterInView}`}
                 closedIcon="fa-ellipsis-h"
                 ariaLabel="Blog Menu"
             />
         </CloseOnOutsideClick>
     );
 };
+
+const useOffsetByFooter = () => {
+    const [mightBeFooterInView, shouldAddFooterInView] = useAddCssClass("footer-in-view");
+    const debouncedSetMenuHeightOffset = useFunctionThrottle(() => setMenuButtonHeightOffset(shouldAddFooterInView), 500);
+    
+    useEffect(() => {
+        window.addEventListener("scroll", debouncedSetMenuHeightOffset);
+        return () => window.removeEventListener("scroll", debouncedSetMenuHeightOffset);
+    }, []);
+
+    return mightBeFooterInView;
+}
+
+const setMenuButtonHeightOffset = shouldAddFooterInView => {
+    const isFooterInViewport = document.documentElement.scrollTop >= getFooterInViewportStartingPixel();
+    shouldAddFooterInView(isFooterInViewport);
+}
+const getFooterInViewportStartingPixel = () => document.body.offsetHeight - getViewportSize() - MAX_FOOTER_HEIGHT.replace("px", "");
 
 export default ResponsiveBlogMenu;
