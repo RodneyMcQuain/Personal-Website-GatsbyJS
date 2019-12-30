4a8ab17c3f451586a047d8984858e1dc40563283
Rodney McQuain <rodneymcquain95@gmail.com>
2019-12-29 19:56:11 -0500

    feat: make preloader a shine

--- src/components/Layout/Preloader.js
+++ src/components/Layout/Preloader.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Preloader = () => (
     <div id="preloader-overlay">
-        <div id="preloader-spinner">
-            <span className="sr-only">Loading...</span>
+        <div id="preloader-wrapper">
+            <div id="preloader-spinner">
+                <span className="sr-only">Loading...</span>
+            </div>
         </div>
     </div>
 );
