199fa857ff64525dd1189678fc747cb1d9b423e2
Rodney McQuain <rodneymcquain95@gmail.com>
2020-01-11 20:12:38 -0500

    fix: wrap tag filter pills on overflow

--- src/components/Blog/TagPills.tsx
+++ src/components/Blog/TagPills.tsx
@@ -9,7 +9,7 @@ const TagPills = ({ tagFilters, setTagFilters }: IPillsProps) => (
     <ul className="pills">
         {tagFilters.map(thisTag => (            
             <li onClick={() => setTagFilters(tagFilters.filter(tagFilter => thisTag !== tagFilter))}>
-                <span className="pill" aria-label={`Remove ${thisTag} tag filter`}>
+                <span aria-label={`Remove ${thisTag} tag filter`}>
                     {thisTag} <span className="fa fa-times-circle"></span>
                 </span>
             </li>
