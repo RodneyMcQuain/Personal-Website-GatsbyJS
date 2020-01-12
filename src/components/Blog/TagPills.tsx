9706c76d843cb38284c6b04fce550876f9f1511b
Rodney McQuain <rodneymcquain95@gmail.com>
2020-01-12 17:21:17 -0500

    feat: use common button styling for pills

--- src/components/Blog/TagPills.tsx
+++ src/components/Blog/TagPills.tsx
@@ -9,7 +9,7 @@ const TagPills = ({ tagFilters, setTagFilters }: IPillsProps) => (
     <ul className="pills">
         {tagFilters.map(thisTag => (            
             <li onClick={() => setTagFilters(tagFilters.filter(tagFilter => thisTag !== tagFilter))}>
-                <span aria-label={`Remove ${thisTag} tag filter`}>
+                <span className="btn" aria-label={`Remove ${thisTag} tag filter`}>
                     {thisTag} <span className="fa fa-times-circle"></span>
                 </span>
             </li>
