80a564d2c22207a9961605988ba9087f5e11d3cc
Rodney McQuain <rodneymcquain95@gmail.com>
2020-02-18 22:55:14 -0500

    feat: add labels to previous/next posts

--- src/components/Blog/Post/AdjacentPostNavigation.js
+++ src/components/Blog/Post/AdjacentPostNavigation.js
@@ -3,10 +3,34 @@ import Link from 'gatsby-link';
 import Icon from '../../Icon';
 
 const AdjacentPostNavigation = ({ previous, next }) => (
-    <div className="adjacent-navigation-container">
-        {next ? <Link to={next.path}><Icon icon="fa fa-long-arrow-left" /> {next.title}</Link> : <EmptyDiv />}
-        {previous ? <Link to={previous.path}>{previous.title} <Icon icon="fa fa-long-arrow-right" /></Link> : <EmptyDiv />}
-    </div>
+    <nav className="adjacent-navigation-container">
+        {
+            next 
+                ? (
+                    <Link className="next" to={next.path}>
+                        <div id="next-post" className="-small-text -gray-text">Next Post</div>
+                        <div className="adjacent-navigation" aria-labelledby="next-post">
+                            <div><Icon icon="fa fa-long-arrow-left" /></div> 
+                            &nbsp;<div>{next.title}</div>
+                        </div>
+                    </Link>
+                ) 
+                : <EmptyDiv />
+        }
+        {
+            previous 
+                ? (
+                    <Link className="previous" to={previous.path}>
+                        <div id="previous-post" className="-small-text -gray-text">Previous Post</div>
+                        <div className="adjacent-navigation" aria-labelledby="previous-post">
+                            <div>{previous.title}</div> 
+                            &nbsp;<div><Icon icon="fa fa-long-arrow-right" /></div>
+                        </div>
+                    </Link> 
+                )
+                : <EmptyDiv />
+        }
+    </nav>
 );
 
 const EmptyDiv = () => <div aria-hidden="true"></div>;
