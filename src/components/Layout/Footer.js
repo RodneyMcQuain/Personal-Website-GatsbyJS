02540bc9d47137c4b38fd9013742c048d2ae5d49
Rodney McQuain <rodneymcquain95@gmail.com>
2020-02-22 00:18:44 -0500

    feat: make footer spacing match the navbar's

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,17 +3,15 @@ import Icon from '../Icon';
 
 const Footer = () => (
     <footer className="dark-section">
-        <div className="container">
-            <p className="footer-copyright">© {new Date().getFullYear()} Copyright: Rodney McQuain</p>
+        <p className="footer-copyright">© {new Date().getFullYear()} Copyright: Rodney McQuain</p>
 
-            <div className="footer-icons">
-                <a className="btn footer-icon" href="https://github.com/RodneyMcQuain/" aria-label="Go to my GitHub" title="My GitHub">
-                    <Icon icon="fa fa-github" />
-                </a>
-                <a className="btn footer-icon" href="https://www.linkedin.com/in/rodneymcquain/" aria-label="Go to my LinkedIn" title="My LinkedIn">
-                    <Icon icon="fa fa-linkedin" />
-                </a>
-            </div>
+        <div className="footer-icons">
+            <a className="btn footer-icon" href="https://github.com/RodneyMcQuain/" aria-label="Go to my GitHub" title="My GitHub">
+                <Icon icon="fa fa-github" />
+            </a>
+            <a className="btn footer-icon" href="https://www.linkedin.com/in/rodneymcquain/" aria-label="Go to my LinkedIn" title="My LinkedIn">
+                <Icon icon="fa fa-linkedin" />
+            </a>
         </div>
     </footer>
 );
