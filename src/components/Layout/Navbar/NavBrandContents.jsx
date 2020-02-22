654d9f3761d01348b8b05f984de792ce709ae3af
Rodney McQuain <rodneymcquain95@gmail.com>
2020-02-22 10:08:13 -0500

    feat: add box shadow to brand dropdown

--- src/components/Layout/Navbar/NavBrandContents.jsx
+++ src/components/Layout/Navbar/NavBrandContents.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import NavItem from './NavItem';
 
-const NavBrandContents = ({ isOpen }) => {
-    return (
-        <ul className={`brand-dropdown${isOpen ? ' open-dropdown' : ''}`}>
-            <NavItem text="About Me" url="#about-me" icon="fa fa-user" />
-            <NavItem text="Education" url="#education" icon="fa fa-graduation-cap" />
-            <NavItem text="Skills" url="#skills" icon="fa fa-list-alt" />
-        </ul>
-    );
-};
+const NavBrandContents = ({ isOpen }) => (
+    <ul className={`brand-dropdown -layered-box-shadow${isOpen ? ' open-dropdown' : ''}`}>
+        <NavItem text="About Me" url="#about-me" icon="fa fa-user" />
+        <NavItem text="Education" url="#education" icon="fa fa-graduation-cap" />
+        <NavItem text="Skills" url="#skills" icon="fa fa-list-alt" />
+    </ul>
+);
 
 export default NavBrandContents;
