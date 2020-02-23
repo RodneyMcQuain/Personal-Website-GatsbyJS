fc9de8c1ece70d7d475e1f21a82634691a2021ab
Rodney McQuain <rodneymcquain95@gmail.com>
2020-02-23 10:08:52 -0500

    feat: give more descriptive ARIA label for GitHub button

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Img from "gatsby-image";
 import Icon from '../Icon';
 
-const Project = ({project}) => (
+const Project = ({ project }) => (
     <div className="col-lg-4 col-md-4 col-sm-6 col-xs-12 project-container margin-container upscale-container">
         <div className="-curved-border -layered-box-shadow">
             <Img fluid={project.image.childImageSharp.fluid} className="container-top-image" alt={`${project.title} project`} />
@@ -12,8 +12,8 @@ const Project = ({project}) => (
                 </ul>
                 <h2 className="-top-margin">{project.title}</h2>
                 <p>{project.description}</p>
-                <a className="btn" href={project.gitHubUrl} aria-label="Go to GitHub">
-                    <span><Icon icon="fa fa-github" /> GitHub</span>
+                <a className="btn" href={project.gitHubUrl} aria-label={`Go to GitHub for my ${project.title} project`}>
+                    <Icon icon="fa fa-github" /> GitHub
                 </a>
             </div>
         </div>
