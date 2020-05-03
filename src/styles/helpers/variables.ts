8f1b781dd0f13530cbee8f838510b40bfb56866a
Rodney McQuain <rodneymcquain95@gmail.com>
2020-05-02 20:57:51 -0400

    clean: abstract common get unitless measurement operation for SCSS to JS mapping

--- src/styles/helpers/variables.ts
+++ src/styles/helpers/variables.ts
@@ -1,6 +1,10 @@
 import { NAVBAR_HEIGHT, LETTER_ANIMATION_DURATION, FEATURED_IMG_HEIGHT, MAX_FOOTER_HEIGHT } from './_variables.scss';
 
-export const NAVBAR_HEIGHT_PIXELS = Number(NAVBAR_HEIGHT?.replace("px", ""));
-export const LETTER_ANIMATION_DURATION_MS = Number(LETTER_ANIMATION_DURATION?.replace("ms", ""));
-export const FEATURED_IMG_VIEWPORT_HEIGHT = Number(FEATURED_IMG_HEIGHT?.replace("vh", ""));
-export const MAX_FOOTER_PIXELS = Number(MAX_FOOTER_HEIGHT?.replace("px", ""));
+export const NAVBAR_HEIGHT_PIXELS = getUnitlessMeasurement(NAVBAR_HEIGHT, "px");
+export const MAX_FOOTER_PIXELS = getUnitlessMeasurement(MAX_FOOTER_HEIGHT, "px");
+export const LETTER_ANIMATION_DURATION_MS = getUnitlessMeasurement(LETTER_ANIMATION_DURATION, "ms");
+export const FEATURED_IMG_VIEWPORT_HEIGHT = getUnitlessMeasurement(FEATURED_IMG_HEIGHT, "vh");
+
+function getUnitlessMeasurement(measurement: string, unit: string): number {
+    return Number(measurement?.replace(unit, ""));
+}
