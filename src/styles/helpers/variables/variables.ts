630c03a5b10f85fe4713fda1a865f47e7028387a
Rodney McQuain <rodneymcquain95@gmail.com>
2020-05-03 10:16:28 -0400

    fix: export correct variable name for max footer height pixels

--- src/styles/helpers/variables/variables.ts
+++ src/styles/helpers/variables/variables.ts
@@ -1,7 +1,7 @@
 import { NAVBAR_HEIGHT, LETTER_ANIMATION_DURATION, FEATURED_IMG_HEIGHT, MAX_FOOTER_HEIGHT } from './_variables.scss';
 
 export const NAVBAR_HEIGHT_PIXELS = getUnitlessMeasurement(NAVBAR_HEIGHT, "px");
-export const MAX_FOOTER_PIXELS = getUnitlessMeasurement(MAX_FOOTER_HEIGHT, "px");
+export const MAX_FOOTER_HEIGHT_PIXELS = getUnitlessMeasurement(MAX_FOOTER_HEIGHT, "px");
 export const LETTER_ANIMATION_DURATION_MS = getUnitlessMeasurement(LETTER_ANIMATION_DURATION, "ms");
 export const FEATURED_IMG_VIEWPORT_HEIGHT = getUnitlessMeasurement(FEATURED_IMG_HEIGHT, "vh");
 
