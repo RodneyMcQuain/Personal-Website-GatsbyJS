ca7cdda6f60b9836c1cf4810aa7b9dd6a0faad5d
Rodney McQuain <rodneymcquain95@gmail.com>
2020-06-14 14:48:50 -0400

    feat: increase readability of skills section

--- src/components/HomePage/Skills/SkillSection.js
+++ src/components/HomePage/Skills/SkillSection.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const SkillSection = ({skillSection}) => (
+const SkillSection = ({ skillSection: { title, skills} }) => (
     <div className="-top-margin col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <h2>{skillSection.title}</h2>
-        <ul className="technologies">
-            {skillSection.skills.map(skill => <li key={skill}>{skill}</li>)}
+        <h2 className="-left-align skill-section-title">{title}</h2>
+        <ul className="skill-section-list">
+            {skills.map(skill => <li className="item -left-align" key={skill}>{skill}</li>)}
         </ul>
     </div>
 );
