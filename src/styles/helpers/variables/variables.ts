f172c30dd6115eab9d3cb93347ee26879bff27c8
Rodney McQuain <rodneymcquain95@gmail.com>
2020-07-25 09:51:37 -0400

    wip: add pixel fallback for rem units

--- src/styles/helpers/variables/variables.ts
+++ src/styles/helpers/variables/variables.ts
@@ -1,7 +1,7 @@
 import { NAVBAR_HEIGHT, LETTER_ANIMATION_DURATION, FEATURED_IMG_HEIGHT, MAX_FOOTER_HEIGHT } from './_variables.scss';
 
-export const NAVBAR_HEIGHT_REM = getUnitlessMeasurement(NAVBAR_HEIGHT, "rem");
-export const MAX_FOOTER_HEIGHT_REM = getUnitlessMeasurement(MAX_FOOTER_HEIGHT, "rem");
+export const NAVBAR_HEIGHT_REM = NAVBAR_HEIGHT;
+export const MAX_FOOTER_HEIGHT_REM = MAX_FOOTER_HEIGHT;
 export const LETTER_ANIMATION_DURATION_MS = getUnitlessMeasurement(LETTER_ANIMATION_DURATION, "ms");
 export const FEATURED_IMG_VIEWPORT_HEIGHT = getUnitlessMeasurement(FEATURED_IMG_HEIGHT, "vh");
 
