10a3cbce3cc118dbba6b53e4191e149dcd270bff
Rodney McQuain <rodneymcquain95@gmail.com>
2020-09-30 19:55:48 -0400

    fix: use correct path for "All Posts" link

--- src/components/Blog/Post/PostsList.js
+++ src/components/Blog/Post/PostsList.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import { BLOG_ROUTE } from '../../../services/routes';
 
-const PostsList = ({posts, currentPostName}) => (
+const PostsList = ({ posts, currentPostName }) => (
     <ul>
-        <li key={0}><Link to={'blog-posts'}>
-            <span>All Posts</span>
-        </Link></li>
+        <li key={0}>
+            <Link to={`/${BLOG_ROUTE}`}>
+                All Posts
+            </Link>
+        </li>
 
         {posts.map(post => {
             const { node } = post;
             const { title, path } = node.frontmatter;
 
-            return title === currentPostName 
+            return title === currentPostName
                 ? null
                 : (
-                    <li key={node.id}><Link to={path}>
-                        <span>{title}</span>
-                    </Link></li>
+                    <li key={node.id}>
+                        <Link to={path}>
+                            {title}
+                        </Link>
+                    </li>
                 );
         })}
     </ul>
