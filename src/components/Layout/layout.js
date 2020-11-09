781aaae82714eda7515df5ef497edc88bc7b153f
Rodney McQuain <rodneymcquain95@gmail.com>
2020-11-08 23:08:48 -0500

    feat: add skip to content link

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar/Navbar';
 import '../../styles/stylesheet.scss';
+import '../../styles/layout/_skip-to-content.scss';
+
+const MAIN_CONTENT_ID = 'main-content';
 
 const Layout = ({ children }) => (
     <>
+        <SkipToContent />
         <Navbar />
-        <main>{children}</main>
+        <main id={MAIN_CONTENT_ID}>{children}</main>
         <Footer />
     </>
+);
+
+const SkipToContent = () => (
+    <a href={`#${MAIN_CONTENT_ID}`} className={"skip-to-content -layered-box-shadow"}>Skip to Content</a>
 );
 
 export default Layout;
