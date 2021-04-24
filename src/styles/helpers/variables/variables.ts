e8ef9d62f520b899de341f5085562340b32756f0
Rodney McQuain <rodneymcquain95@gmail.com>
2021-04-24 14:19:09 -0400

    clean: import SCSS variables to TS properly

--- src/styles/helpers/variables/variables.ts
+++ src/styles/helpers/variables/variables.ts
@@ -1,10 +1,10 @@
-import { NAVBAR_HEIGHT, FEATURED_IMG_HEIGHT, MAX_FOOTER_HEIGHT } from './_variables.scss';
-import { LETTER_ANIMATION_DURATION } from '../../layout/components/Layout/Navbar/Brand/AnimatedLetter.module.scss';
+import { navbarHeight, featuredImageHeight, maxFooterHeight } from './variables.module.scss';
+import { letterAnimationDuration } from '../../layout/components/Layout/Navbar/Brand/AnimatedLetter.module.scss';
 
-export const NAVBAR_HEIGHT_REM = getUnitlessMeasurement(NAVBAR_HEIGHT, "rem");
-export const MAX_FOOTER_HEIGHT_REM = getUnitlessMeasurement(MAX_FOOTER_HEIGHT, "rem");
-export const LETTER_ANIMATION_DURATION_MS = getUnitlessMeasurement(LETTER_ANIMATION_DURATION, "ms");
-export const FEATURED_IMG_VIEWPORT_HEIGHT = getUnitlessMeasurement(FEATURED_IMG_HEIGHT, "vh");
+export const NAVBAR_HEIGHT_REM = getUnitlessMeasurement(navbarHeight, "rem");
+export const MAX_FOOTER_HEIGHT_REM = getUnitlessMeasurement(maxFooterHeight, "rem");
+export const LETTER_ANIMATION_DURATION_MS = getUnitlessMeasurement(letterAnimationDuration, "ms");
+export const FEATURED_IMG_VIEWPORT_HEIGHT = getUnitlessMeasurement(featuredImageHeight, "vh");
 
 function getUnitlessMeasurement(measurement: string, unit: string): number {
     return Number(measurement?.replace(unit, ""));
