c67a63915753dff1aeb682b79c07c0eabade6ce3
Rodney McQuain <rodneymcquain95@gmail.com>
2021-07-03 20:04:42 -0400

    feat: update resume page for June 2021

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf';
 import Layout from '../components/Layout/layout';
 import SEO from '../components/Shared/SEO';
-import { SizeMe } from 'react-sizeme';
 import IconText from '../components/Shared/IconText';
 import { FaDownload } from '@meronex/icons/fa';
 import HeaderContentLayout from '../components/Layout/HeaderContentLayout';
 
-const RESUME_FILE_NAME = "documents/Rodney McQuain - Resume 12-10-2018.pdf";
+const RESUME_FILE_PATH = "documents/Rodney McQuain - Resume - June 2021.pdf";
 
 const Resume = () => (
     <Layout>
         <SEO title="Resume" shouldNotIndex />
         <HeaderContentLayout title="Resume">
-            <SizeMe>
-                {({ size }) => (
-                    <Document file={RESUME_FILE_NAME}>
-                        <Page
-                            pageNumber={1}
-                            width={size.width}
-                        />
-                    </Document>
-                )}
-            </SizeMe>
-
-            <a className="btn -secondary-text" href={RESUME_FILE_NAME} download>
+            <embed src={RESUME_FILE_PATH} width="100%" height="800rem" />
+            <br />
+            <a className="btn -secondary-text" href={RESUME_FILE_PATH} download>
                 <IconText icon={<FaDownload />} text="Download My Resume" />
             </a>
         </HeaderContentLayout>
