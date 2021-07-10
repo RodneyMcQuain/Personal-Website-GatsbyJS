8f71b84c5ec12cbf17698767e877f3582a2e74e8
Rodney McQuain <rodneymcquain95@gmail.com>
2021-07-10 19:23:34 -0400

    clean: re-organize verbose plugins into functions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,3 @@
-
 const {
     NODE_ENV,
     URL: NETLIFY_SITE_URL = 'https://www.rodneymcquain.com',
@@ -25,145 +24,183 @@ module.exports = {
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-typescript`,
         `gatsby-plugin-tslint`,
-        {
-            resolve: `gatsby-plugin-netlify`,
-            options: {
-                headers: {
-                    "/*": [ "X-Frame-Options: SAMEORIGIN" ]
-                }
-            },
-        },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {
-                cssLoaderOptions: {
-                    modules: {
-                        exportLocalsConvention: 'camelCaseOnly',
-                    },
-                },
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Rodney McQuain: Personal Site`,
-                short_name: `Rodney McQuain`,
-                start_url: `/`,
-                background_color: `#222`,
-                theme_color: `#222`,
-                display: `standalone`,
-                icon: `src/assets/svg/round-square-logo.svg`,
-            }
-        },
-        {
-            resolve: `gatsby-plugin-offline`,
-            options: {
-                precachePages: [
-                    `*/blog-posts/*`
-                ]
-            }
-        },
-        {
-            resolve: `gatsby-plugin-sitemap`,
-            options: {
-                query: allSitesQuery(),
-                serialize: generateSitemap,
-            }
-        },
-        {
-            resolve: `gatsby-plugin-robots-txt`,
-            options: {
-                resolveEnv: () => NETLIFY_ENV,
-                env: {
-                    production: {
-                        policy: [{ userAgent: '*' }]
-                    },
-                    'branch-deploy': {
-                        policy: [{ userAgent: '*', disallow: ['/'] }],
-                        sitemap: null,
-                        host: null
-                    },
-                    'deploy-preview': {
-                        policy: [{ userAgent: '*', disallow: ['/'] }],
-                        sitemap: null,
-                        host: null
-                    },
-                },
-            },
-        },
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: "UA-155137763-1",
-                head: true,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/data`,
-                name: `data`
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/pages`,
-                name: `pages`,
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    `gatsby-remark-autolink-headers`,
-                    `gatsby-remark-prismjs`,
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 1200,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-copy-linked-files`,
-                        options: {
-                            ignoreFileExtensions: [`png`, `jpg`, `jpeg`],
-                        },
-                    },
-                ],
-            },
-        },
+        gatsbyPluginNetlify(),
+        gatsbyPluginSass(),
+        gatsbyPluginManifest(),
+        gatsbyPluginOffline(),
+        gatsbyPluginSitemap(),
+        gatsbyPluginRobotsTxt(),
+        gatsbyPluginGoogleAnalytics(),
+        gatsbySourceFilesystemData(),
+        gatsbySourceFilesystemPages(),
+        gatsbyTransformerRemark(), 
     ],
 }
 
-function allSitesQuery() {
-    return `
-        {
-            site {
-                siteMetadata {
-                    siteUrl
-                }
+function gatsbyPluginNetlify() {
+    return ({
+        resolve: `gatsby-plugin-netlify`,
+        options: {
+            headers: {
+                "/*": [ "X-Frame-Options: SAMEORIGIN" ]
             }
+        },
+    });
+}
 
-            allSitePage {
-                edges {
-                    node {
-                        path
+function gatsbyPluginSass() {
+    return ({
+        resolve: `gatsby-plugin-sass`,
+        options: {
+            cssLoaderOptions: {
+                modules: {
+                    exportLocalsConvention: 'camelCaseOnly',
+                },
+            },
+        },
+    });
+}
+
+function gatsbyPluginManifest() {
+    return ({
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+            name: `Rodney McQuain: Personal Site`,
+            short_name: `Rodney McQuain`,
+            start_url: `/`,
+            background_color: `#222`,
+            theme_color: `#222`,
+            display: `standalone`,
+            icon: `src/assets/svg/round-square-logo.svg`,
+        },
+    });
+}
+
+function gatsbyPluginOffline() {
+    return ({
+        resolve: `gatsby-plugin-offline`,
+        options: { 
+            precachePages: [`*/blog-posts/*`], 
+        },
+    });
+}
+
+function gatsbyPluginSitemap() {
+    return ({
+        resolve: `gatsby-plugin-sitemap`,
+        options: {
+            query: allSitesQuery(),
+            serialize: generateSitemap,
+        }
+    });
+
+    function allSitesQuery() {
+        return `
+            {
+                site {
+                    siteMetadata {
+                        siteUrl
+                    }
+                }
+    
+                allSitePage {
+                    edges {
+                        node {
+                            path
+                        }
                     }
                 }
             }
-        }
-    `;
+        `;
+    }
+    
+    function generateSitemap({ site, allSitePage }) {
+        const BLOG_POST_URL = '/blog-posts/';
+        const DEFAULT_PRIORITY = 0.5;
+        return allSitePage.edges.map(
+            ({ node: { path }}) => ({
+                url: site.siteMetadata.siteUrl + path,
+                changefreq: `daily`,
+                priority: path.includes(BLOG_POST_URL) && !path.endsWith(BLOG_POST_URL) ? DEFAULT_PRIORITY + 0.2 : DEFAULT_PRIORITY,
+            })
+        );
+    }
 }
 
-function generateSitemap({ site, allSitePage }) {
-    const BLOG_POST_URL = '/blog-posts/';
-    const DEFAULT_PRIORITY = 0.5;
-    return allSitePage.edges.map(
-        ({ node: { path }}) => ({
-            url: site.siteMetadata.siteUrl + path,
-            changefreq: `daily`,
-            priority: path.includes(BLOG_POST_URL) && !path.endsWith(BLOG_POST_URL) ? DEFAULT_PRIORITY + 0.2 : DEFAULT_PRIORITY,
-        })
-    );
+function gatsbyPluginRobotsTxt() {
+    return ({
+        resolve: `gatsby-plugin-robots-txt`,
+        options: {
+            resolveEnv: () => NETLIFY_ENV,
+            env: {
+                production: {
+                    policy: [{ userAgent: '*' }],
+                },
+                'branch-deploy': {
+                    policy: [{ userAgent: '*', disallow: ['/'] }],
+                    sitemap: null,
+                    host: null,
+                },
+                'deploy-preview': {
+                    policy: [{ userAgent: '*', disallow: ['/'] }],
+                    sitemap: null,
+                    host: null,
+                },
+            },
+        },
+    });
+}
+
+function gatsbyPluginGoogleAnalytics() {
+    return ({
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+            trackingId: "UA-155137763-1",
+            head: true,
+        },
+    });
+}
+
+function gatsbySourceFilesystemData() {
+    return ({
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            path: `${__dirname}/src/data`,
+            name: `data`,
+        },
+    });
+}
+
+function gatsbySourceFilesystemPages() {
+    return ({
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            path: `${__dirname}/src/pages`,
+            name: `pages`,
+        },
+    });
+}
+
+function gatsbyTransformerRemark() {
+    return ({
+        resolve: `gatsby-transformer-remark`,
+        options: {
+            plugins: [
+                `gatsby-remark-autolink-headers`,
+                `gatsby-remark-prismjs`,
+                {
+                    resolve: `gatsby-remark-images`,
+                    options: { 
+                        maxWidth: 1200, 
+                    },
+                },
+                {
+                    resolve: `gatsby-remark-copy-linked-files`,
+                    options: {
+                        ignoreFileExtensions: [`png`, `jpg`, `jpeg`],
+                    },
+                },
+            ],
+        },
+    });
 }
