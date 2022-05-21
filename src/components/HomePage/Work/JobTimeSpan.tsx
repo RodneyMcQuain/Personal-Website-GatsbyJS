59585f43a021d65622261eadcd1979919755555f
Rodney McQuain <rodneymcquain95@gmail.com>
2022-05-21 12:08:35 -0400

    feat: support year representation in work experience time span

--- src/components/HomePage/Work/JobTimeSpan.tsx
+++ src/components/HomePage/Work/JobTimeSpan.tsx
@@ -2,27 +2,42 @@ import React from 'react';
 
 interface JobTimeSpanProps {
     startDate: string;
-    endDate: string;
+    endDate: string | null;
 }
 
-const JobTimeSpan = ({ startDate, endDate }: JobTimeSpanProps) => {
-    const monthDifference = getMonthDifference(startDate, endDate);
-    return (
-        <div className="-small-text -gray-text">
-            {startDate} – {endDate ?? "Present"} ~ {monthDifference} Month{monthDifference === 1 ? '' : 's'}
-        </div>
-    );
-}
+const JobTimeSpan = ({ startDate, endDate }: JobTimeSpanProps) => (
+    <div className="-small-text -gray-text">{startDate} – {endDate ?? "Present"} ~ {getTimeWorked(startDate, endDate)}</div>
+);
 
-const getMonthDifference = (from: string, to: string): number => {
-    const NUMBER_OF_MONTHS_IN_YEAR = 12;
+const getTimeWorked = (from: string, to: string | null): string => {
     const fromDate = new Date(from);
 
     to = to ?? new Date().toString();
     const toDate = new Date(to);
 
-    const yearDifferenceInMonths = (toDate.getFullYear() - fromDate.getFullYear()) * NUMBER_OF_MONTHS_IN_YEAR;
-    return yearDifferenceInMonths - fromDate.getMonth() + toDate.getMonth();
+    return getFormattedTimeWorked(fromDate, toDate);
+}
+
+const NUMBER_OF_MONTHS_IN_YEAR = 12;
+
+const getFormattedTimeWorked = (startDate: Date, endDate: Date): string => {
+    const monthDifference = getTotalMonthDifference(startDate, endDate);
+    
+    const yearCount = Math.floor(monthDifference / NUMBER_OF_MONTHS_IN_YEAR);
+    if (yearCount === 0) return getMonthFormatted(monthDifference);
+
+    const monthsIntoMostRecentYear = monthDifference % NUMBER_OF_MONTHS_IN_YEAR;
+    if (monthsIntoMostRecentYear === 0) return `${getYearFormatted(yearCount)}`;
+
+    return `${getYearFormatted(yearCount)} ${getMonthFormatted(monthsIntoMostRecentYear)}`; 
+};
+
+const getYearFormatted = (numberOfYears: number): string => `${numberOfYears} Year${numberOfYears === 1 ? '' : 's'}`;
+const getMonthFormatted = (numberOfMonths: number): string => `${numberOfMonths} Month${numberOfMonths === 1 ? '' : 's'}`;
+
+const getTotalMonthDifference = (startDate: Date, endDate: Date): number => {
+    const yearDifferenceInMonths = (endDate.getFullYear() - startDate.getFullYear()) * NUMBER_OF_MONTHS_IN_YEAR;
+    return yearDifferenceInMonths + (endDate.getMonth() - startDate.getMonth());
 };
 
 export default JobTimeSpan;
