ae2459654270cba657c2a072767196c690c5a73d
Rodney McQuain <rodneymcquain95@gmail.com>
2022-09-05 11:32:09 -0400

    feat: add notable points to projects

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -6,18 +6,19 @@ import ExternalLinkButton from '../Shared/ExternalLinkButton';
 import { FaGithub } from '@meronex/icons/fa';
 import ReadBlogIcon from '../Blog/Icons/ReadBlogIcon';
 import MdcWeb from '@meronex/icons/mdc/MdcWeb';
-import { projectContainer, projectLinkContainer, languageTag } from '../../styles/layout/components/Projects/Project.module.scss';
+import { projectContainer, projectLinkContainer, languageTag, notablePoints as notablePointsClass} from '../../styles/layout/components/Projects/Project.module.scss';
 
-const Project = ({ project: { image, title, technologies, description, gitHubUrl, siteUrl, associatedBlogPostMetadata } }) => (
+const Project = ({ project: { image, title, technologies, description, gitHubUrl, siteUrl, associatedBlogPostMetadata, notablePoints } }) => (
     <ImageTopCard
         image={image.childImageSharp.fluid}
         imageAltText={`${title} project`}
-        className={`${projectContainer} col-lg-4 col-md-4 col-sm-6 col-xs-12 margin-container upscale-container`}
+        className={`${projectContainer} col-lg-6 col-md-6 col-sm-6 col-xs-12 margin-container upscale-container`}
         hasInnerMargin
     >
         <LanguageTags technologies={technologies} />
         <h2>{title}</h2>
         <p>{description}</p>
+        {notablePoints && <ul className={notablePointsClass}>{notablePoints.map(notablePoint => <li>{notablePoint}</li>)}</ul>}
         <div className={projectLinkContainer}>
             {siteUrl && (
                 <ExternalLinkButton href={siteUrl} ariaLabel={`Go to the live site for ${title}`}>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -39,6 +39,7 @@ const ProjectsStaticQuery = () => (
                             gitHubUrl
                             siteUrl
                             associatedBlogPostPath
+                            notablePoints
                         }
                     }
                 }
